92320c710c18b60ececa5b091f627a14b0a2c273
JoshuaLeninTan <113226903+JoshuaLeninTan@users.noreply.github.com>
2024-04-15 18:37:26 +0800

    Updated

---
+++ SenseTest_w_Case1.m
@@ -0,0 +1,221 @@
+x1 = optimvar('x1', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+x2 = optimvar('x2', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+x3 = optimvar('x3', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+x4 = optimvar('x4', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+x5 = optimvar('x5', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+x6 = optimvar('x6', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+x7 = optimvar('x7', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+x8 = optimvar('x8', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+x9 = optimvar('x9', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+x10 = optimvar('x10', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+x11 = optimvar('x11', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+x12 = optimvar('x12', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+x13 = optimvar('x13', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+
+% variables for condition when there is no meat
+m1 = optimvar('m1', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+m2 = optimvar('m2', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+m3 = optimvar('m3', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+m4 = optimvar('m4', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+m5 = optimvar('m5', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+m6 = optimvar('m6', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+m7 = optimvar('m7', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+m8 = optimvar('m8', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+m9 = optimvar('m9', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+m10 = optimvar('m10', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+m11 = optimvar('m11', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+m12 = optimvar('m12', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+m13 = optimvar('m13', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+
+% variables for condition when there is no fish
+f1 = optimvar('f1', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+f2 = optimvar('f2', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+f3 = optimvar('f3', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+f4 = optimvar('f4', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+f5 = optimvar('f5', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+f6 = optimvar('f6', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+f7 = optimvar('f7', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+f8 = optimvar('f8', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+f9 = optimvar('f9', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+f10 = optimvar('f10', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+f11 = optimvar('f11', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+f12 = optimvar('f12', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+f13 = optimvar('f13', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+
+% variables for condition when there is no veggies
+v1 = optimvar('v1', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+v2 = optimvar('v2', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+v3 = optimvar('v3', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+v4 = optimvar('v4', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+v5 = optimvar('v5', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+v6 = optimvar('v6', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+v7 = optimvar('v7', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+v8 = optimvar('v8', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+v9 = optimvar('v9', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+v10 = optimvar('v10', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+v11 = optimvar('v11', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+v12 = optimvar('v12', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+v13 = optimvar('v13', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+
+% variables for condition when there is no eggs
+e1 = optimvar('e1', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+e2 = optimvar('e2', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+e3 = optimvar('e3', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+e4 = optimvar('e4', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+e5 = optimvar('e5', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+e6 = optimvar('e6', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+e7 = optimvar('e7', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+e8 = optimvar('e8', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+e9 = optimvar('e9', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+e10 = optimvar('e10', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+e11 = optimvar('e11', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+e12 = optimvar('e12', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+e13 = optimvar('e13', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+
+% variables for condition when there is no eggs
+b1 = optimvar('b1', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+b2 = optimvar('b2', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+b3 = optimvar('b3', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+b4 = optimvar('b4', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+b5 = optimvar('b5', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+b6 = optimvar('b6', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+b7 = optimvar('b7', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+b8 = optimvar('b8', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+b9 = optimvar('b9', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+b10 = optimvar('b10', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+b11 = optimvar('b11', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+b12 = optimvar('b12', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+b13 = optimvar('b13', 'Type', 'integer', 'LowerBound', 0, 'UpperBound', Inf);
+
+%prob2 = optimproblem('Objective', 33.0*y1 + 39.0*y2 + 34.0*y3 + 29.5*y4 + 15.0*y5 + 10.5*y6 + 14.0*y7 + 14.0*y8 + .90*y9 + .70*y10 + .70*y11 + 11.0*y12 + 6*y13, 'ObjectiveSense', 'min');
+
+%% Original Problem
+
+%objective equation is multiplied by cost per 100gram to get objective cost
+prob = optimproblem('Objective', 33.0*x1 + 39.0*x2 + 34.0*x3 + 29.5*x4 + 15.0*x5 + 10.5*x6 + 14.0*x7 + 14.0*x8 + .90*x9 + .70*x10 + .70*x11 + 11.0*x12 + 6*x13, 'ObjectiveSense', 'min');
+
+% Protein Lower Limit
+prob.Constraints.c1 = 20.70*x1 + 21.24*x2 + 9.34*x3 + 23.10*x4 + 27.30*x5 + 26.20*x6 + 20.50*x7 + 18.50*x8 + 1.82*x9 + 1.82*x10 + 0.98*x11 + 3.60*x12 + 6.3*x13 >= 350;
+% Budget Constraint
+prob.Constraints.c2 = 33.0*x1 + 39.0*x2 + 34.0*x3 + 29.5*x4 + 15.0*x5 + 10.5*x6 + 14.0*x7 + 14.0*x8 + 9*x9 + 7*x10 + 7*x11 + 11.0*x12 + 6*x13 <= 1690; 
+%constraint for meats
+prob.Constraints.c3 = 100*(x1 + x2 + x3 + x4 + x5) <= 700;%multiplying 100 to get a 100 gram serving of each meat
+%constraint for fish
+prob.Constraints.c4 =100*(x6 + x7 + x8) <= 340;%multiplying 100 to get a 100 gram serving of each fish
+%constraint for veggie
+prob.Constraints.c5 = 100*(x9 + x10 + x11 + x12) <= 180;%multiplying 100 to get a 100 gram serving of each veggie
+%constraint for egg
+prob.Constraints.c6 = 50*x13 <= 1400;%multiplying 50 to get a of each egg
+
+%% Case 1: Quantity Constraints
+
+%objective equation is multiplied by cost per 100gram to get objective cost
+probNM = optimproblem('Objective', 33.0*m1 + 39.0*m2 + 34.0*m3 + 29.5*m4 + 15.0*m5 + 10.5*m6 + 14.0*m7 + 14.0*m8 + .90*m9 + .70*m10 + .70*m11 + 11.0*m12 + 6*m13, 'ObjectiveSense', 'min');
+probNF = optimproblem('Objective', 33.0*f1 + 39.0*f2 + 34.0*f3 + 29.5*f4 + 15.0*f5 + 10.5*f6 + 14.0*f7 + 14.0*f8 + .90*f9 + .70*f10 + .70*f11 + 11.0*f12 + 6*f13, 'ObjectiveSense', 'min');
+probNV = optimproblem('Objective', 33.0*v1 + 39.0*v2 + 34.0*v3 + 29.5*v4 + 15.0*v5 + 10.5*v6 + 14.0*v7 + 14.0*v8 + .90*v9 + .70*v10 + .70*v11 + 11.0*v12 + 6*v13, 'ObjectiveSense', 'min');
+probNE = optimproblem('Objective', 33.0*e1 + 39.0*e2 + 34.0*e3 + 29.5*e4 + 15.0*e5 + 10.5*e6 + 14.0*e7 + 14.0*e8 + .90*e9 + .70*e10 + .70*e11 + 11.0*e12 + 6*e13, 'ObjectiveSense', 'min');
+
+% When no Meat
+% Protein Lower Limit
+probNM.Constraints.c7 = 20.70*m1 + 21.24*m2 + 9.34*m3 + 23.10*m4 + 27.30*m5 + 26.20*m6 + 20.50*m7 + 18.50*m8 + 1.82*m9 + 1.82*m10 + 0.98*m11 + 3.60*m12 + 6.3*m13 >= 350;
+% Budget Constraint
+probNM.Constraints.c8 = 33.0*m1 + 39.0*m2 + 34.0*m3 + 29.5*m4 + 15.0*m5 + 10.5*m6 + 14.0*m7 + 14.0*m8 + 9*m9 + 7*m10 + 7*m11 + 11.0*m12 + 6*m13 <= 1690;
+%constraint for meats
+probNM.Constraints.c9 = 100*(m1 + m2 + m3 + m4 + m5) <= 0;%multiplying 100 to get a 100 gram serving of each meat
+%constraint for fish
+probNM.Constraints.c10 = 100*(m6 + m7 + m8) <= 340;%multiplying 100 to get a 100 gram serving of each fish
+%constraint for veggie
+probNM.Constraints.c11 = 100*(m9 + m10 + m11 + m12) <= 180;%multiplying 100 to get a 100 gram serving of each veggie
+%constraint for egg
+probNM.Constraints.c12 = 50*m13 <= 1400;%multiplying 50 to get a of each egg
+
+% When no Fish:
+% Protein Lower Limit
+probNF.Constraints.c13 = 20.70*f1 + 21.24*f2 + 9.34*f3 + 23.10*f4 + 27.30*f5 + 26.20*f6 + 20.50*f7 + 18.50*f8 + 1.82*f9 + 1.82*f10 + 0.98*f11 + 3.60*f12 + 6.3*f13 >= 350;
+% Budget Constraint
+probNF.Constraints.c14 = 33.0*f1 + 39.0*f2 + 34.0*f3 + 29.5*f4 + 15.0*f5 + 10.5*f6 + 14.0*f7 + 14.0*f8 + 9*f9 + 7*f10 + 7*f11 + 11.0*f12 + 6*f13 <= 1690;
+%constraint for meats
+probNF.Constraints.c15 = 100*(f1 + f2 + f3 + f4 + f5) <= 700;%multiplying 100 to get a 100 gram serving of each meat
+%constraint for fish
+probNF.Constraints.c16 =100*(f6 + f7 + f8) <= 0;%multiplying 100 to get a 100 gram serving of each fish
+%constraint for veggie
+probNF.Constraints.c17 = 100*(f9 + f10 + f11 + f12) <= 180;%multiplying 100 to get a 100 gram serving of each veggie
+%constraint for egg
+probNF.Constraints.c18 = 50*f13 <= 1400;%multiplying 50 to get a of each egg
+
+% When no Veggie
+% Protein Lower Limit
+probNV.Constraints.c19 = 20.70*v1 + 21.24*v2 + 9.34*v3 + 23.10*v4 + 27.30*v5 + 26.20*v6 + 20.50*v7 + 18.50*v8 + 1.82*v9 + 1.82*v10 + 0.98*v11 + 3.60*v12 + 6.3*v13 >= 350;
+% Budget Constraint
+probNV.Constraints.c20 = 33.0*v1 + 39.0*v2 + 34.0*v3 + 29.5*v4 + 15.0*v5 + 10.5*v6 + 14.0*v7 + 14.0*v8 + 9*v9 + 7*v10 + 7*v11 + 11.0*v12 + 6*v13 <= 1690;
+%constraint for meats
+probNV.Constraints.c21 = 100*(v1 + v2 + v3 + v4 + v5) <= 700;%multiplying 100 to get a 100 gram serving of each meat
+%constraint for fish
+probNV.Constraints.c22 =100*(v6 + v7 + v8) <= 340;%multiplying 100 to get a 100 gram serving of each fish
+%constraint for veggie
+probNV.Constraints.c23 = 100*(v9 + v10 + v11 + v12) <= 0;%multiplying 100 to get a 100 gram serving of each veggie
+%constraint for egg
+probNV.Constraints.c24 = 50*v13 <= 1400;%multiplying 50 to get a of each egg
+
+% When no Eggs
+% Protein Lower Limit
+probNE.Constraints.c25 = 20.70*e1 + 21.24*e2 + 9.34*e3 + 23.10*e4 + 27.30*e5 + 26.20*e6 + 20.50*e7 + 18.50*e8 + 1.82*e9 + 1.82*e10 + 0.98*e11 + 3.60*e12 + 6.3*e13 >= 350;
+% Budget Constraint
+probNE.Constraints.c26 = 33.0*e1 + 39.0*e2 + 34.0*e3 + 29.5*e4 + 15.0*e5 + 10.5*e6 + 14.0*e7 + 14.0*e8 + 9*e9 + 7*e10 + 7*e11 + 11.0*e12 + 6*e13 <= 1690;
+%constraint for meats
+probNE.Constraints.c27 = 100*(e1 + e2 + e3 + e4 + e5) <= 700;%multiplying 100 to get a 100 gram serving of each meat
+%constraint for fish
+probNE.Constraints.c28 =100*(e6 + e7 + e8) <= 340;%multiplying 100 to get a 100 gram serving of each fish
+%constraint for veggie
+probNE.Constraints.c29 = 100*(e9 + e10 + e11 + e12) <= 180;%multiplying 100 to get a 100 gram serving of each veggie
+%constraint for egg
+probNE.Constraints.c30 = 50*e13 <= 0;%multiplying 50 to get a of each egg
+
+%% Case 2: 
+%objective equation is multiplied by cost per 100gram to get objective cost
+probB = optimproblem('Objective', 20.70*b1 + 21.24*b2 + 9.34*b3 + 23.10*b4 + 27.30*b5 + 26.20*b6 + 20.50*b7 + 18.50*b8 + 1.82*b9 + 1.82*b10 + 0.98*b11 + 3.60*b12 + 6.3*b13, 'ObjectiveSense', 'max');
+
+% Protein Lower Limit
+probB.Constraints.c31 = 20.70*b1 + 21.24*b2 + 9.34*b3 + 23.10*b4 + 27.30*b5 + 26.20*b6 + 20.50*b7 + 18.50*b8 + 1.82*b9 + 1.82*b10 + 0.98*b11 + 3.60*b12 + 6.3*b13 >= 350;
+% Budget Constraint
+probB.Constraints.c26 = 33.0*b1 + 39.0*b2 + 34.0*b3 + 29.5*b4 + 15.0*b5 + 10.5*b6 + 14.0*b7 + 14.0*b8 + 9*b9 + 7*b10 + 7*b11 + 11.0*b12 + 6*b13 <= 1690;
+%constraint for meats
+probB.Constraints.c33 = 100*(b1 + b2 + b3 + b4 + b5) <= 700;%multiplying 100 to get a 100 gram serving of each meat
+%constraint for fish
+probB.Constraints.c34 =100*(b6 + b7 + b8) <= 340;%multiplying 100 to get a 100 gram serving of each fish
+%constraint for veggie
+probB.Constraints.c35 = 100*(b9 + b10 + b11 + b12) <= 180;%multiplying 100 to get a 100 gram serving of each veggie
+%constraint for egg
+probB.Constraints.c36 = 50*b13 <= 1400;%multiplying 50 to get a of each egg
+
+
+
+%% Solving and Output Section
+% Solve the problem
+[sol, fval] = solve(prob);
+[sol2, fval2] = solve(probNM);
+[sol3, fval3] = solve(probNF);
+[sol4, fval4] = solve(probNV);
+[sol5, fval5] = solve(probNE);
+[sol6, fval6] = solve(probB);
+
+% Display results
+fprintf('Original Problem \n');
+disp(sol);
+disp(fval);
+fprintf('---------------------------------------------------------------------------- \n');
+fprintf('Case1: \n');
+disp(sol2);
+disp(fval2);
+fprintf('--------------------------- \n');
+disp(sol3);
+disp(fval3);
+fprintf('--------------------------- \n');
+disp(sol4);
+disp(fval4);
+fprintf('--------------------------- \n');
+disp(sol5);
+disp(fval5);
+fprintf('--------------------------- \n');
+disp(sol6);
+disp(fval6);
